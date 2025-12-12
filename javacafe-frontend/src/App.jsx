51f1482fec03197817ef0e136959bbd8a844d854
Raman Shrestha <shrestha.raman1126@gmail.com>
2025-12-12 11:16:40 -0600

    added a new routing path to api/menu

--- javacafe-frontend/src/App.jsx
+++ javacafe-frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
+import MenuPage from './pages/MenuPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="min-h-screen bg-amber-50">
             <Navbar />
-            <HomePage />
+            <Routes>
+                <Route path="/api" element={<HomePage />} />
+                <Route path="/api/menu" element={<MenuPage />} />
+            </Routes>
         </div>
     );
 }
