215d1bc3529fe314d7c175a542d3871b5d3d448a
Raman Shrestha <shrestha.raman1126@gmail.com>
2025-12-12 11:57:48 -0600

    finalized menu page routiing and fixed duplicate issues

--- javacafe-frontend/src/App.jsx
+++ javacafe-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-import PlaceOrderPage from './pages/PlaceOrderPage';
 import MenuPage from './pages/MenuPage';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/api" element={<HomePage />} />
                     <Route path="/api/menu" element={<MenuPage />} />
-                    <Route path="/api/cart" element={<PlaceOrderPage />} />
                 </Routes>
 
             </div>
