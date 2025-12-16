b41bd6163df739d5c54381e6ecd877fb38f87247
hg2025 <90288992+hg2025@users.noreply.github.com>
2025-12-16 16:53:54 -0600

    Update JwtUtil.java

--- JavaCafe/src/main/java/com/project1/JavaCafe/JwtUtil.java
+++ JavaCafe/src/main/java/com/project1/JavaCafe/JwtUtil.java
@@ -49,7 +49,7 @@ public class JwtUtil {
     }
     // ----------------------------------------
 
-    // The original single-argument generateToken method is removed.
+   
 
     public boolean validateToken(String token) {
         try {
