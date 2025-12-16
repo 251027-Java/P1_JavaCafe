55243040c47099295185d2ee01b94e80356768fc
hg2025 <90288992+hg2025@users.noreply.github.com>
2025-12-16 17:07:38 -0600

    Update SalesSummaryWOIDDTO.java

--- JavaCafe/src/main/java/com/project1/JavaCafe/DTO/SalesSummaryWOIDDTO.java
+++ JavaCafe/src/main/java/com/project1/JavaCafe/DTO/SalesSummaryWOIDDTO.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 
 public record SalesSummaryWOIDDTO(
-         // Include ID, useful for reporting tools or identifying the record
         LocalDate date,
         int totalOrders,
         int totalItemsSold
